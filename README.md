8eeed6f774279f3695199517d0daf57ba0c3dc02
Lvl3Mage <lvlthreemage@gmail.com>
2024-08-14 03:30:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿[//]: # (/mainpage)
+
+# Coroutine Toolkit
+A package that provides a set of utility functions for working with coroutines in Unity
+## Requirements
+- Unity 2021.3 or later
+
+## Installation
+Include a scoped registry with
+- _name:_ `com.lvl3mage`
+- _url:_ `https://registry.npmjs.org`
+
+Add the package to your Unity project by importing the `com.lvl3mage.coroutine-toolkit` package.
+## Documentation
+The documentation for Coroutine Toolkit can be found [here](https://lvl3mage.github.io/CoroutineToolkit).
+
+## Author
+Lvl3Mage - [GitHub](https://github.com/lvl3mage)
+
+Docs styling with [doxygen-awesome-css](https://github.com/jothepro/doxygen-awesome-css)
---
+++ Runtime/CoroutineRunner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Lvl3Mage.CoroutineToolkit
+{
+	public static class CoroutineRunner
+	{
+		static CoroutineRunnerBehaviour instance;
+		static CoroutineRunnerBehaviour AccessInstance()
+		{
+			if (instance != null) return instance;
+			
+			instance = new GameObject("CoroutineRunner").AddComponent<CoroutineRunnerBehaviour>();
+			Object.DontDestroyOnLoad(instance.gameObject);
+			return instance;
+		}
+		/// <summary>
+		/// Allows a non-monobehaviour class to start a coroutine
+		/// </summary>
+		/// <param name="method">The coroutine to start, passed in as an IEnumerator</param>
+		/// <returns>The started coroutine</returns>
+		public static Coroutine StartCoroutine(IEnumerator method)
+		{
+			return AccessInstance().StartCoroutine(method);
+		}
+		/// <summary>
+		/// Allows a non-monobehaviour class to stop a coroutine
+		/// </summary>
+		/// <param name="coroutine">The coroutine to stop</param>
+		public static void StopCoroutine(Coroutine coroutine)
+		{
+			AccessInstance().StopCoroutine(coroutine);
+		}
+	}
+	/// <summary>
+	/// The MonoBehaviour that actually runs the coroutines
+	/// </summary>
+	/// <remarks>
+	///	Don't use this class directly, use <see cref="CoroutineRunner"/> instead
+	/// </remarks>
+	public class CoroutineRunnerBehaviour : MonoBehaviour
+	{
+		void OnDestroy()
+		{
+			StopAllCoroutines();
+		}
+	}
+}

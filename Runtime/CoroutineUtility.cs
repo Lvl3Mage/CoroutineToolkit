7f05df0edfec42c37d606107a59b6e6cf3f05257
Lvl3Mage <lvlthreemage@gmail.com>
2024-11-06 07:34:25 +0100

    Added more utility methods

--- Runtime/CoroutineUtility.cs
+++ Runtime/CoroutineUtility.cs
@@ -6,7 +6,53 @@ namespace Lvl3Mage.CoroutineToolkit
 {
 	public class CoroutineUtility
 	{
-		public static IEnumerator WaitAll(Coroutine[] coroutines){
+		public static IEnumerator WaitWithInterrupt(IEnumerator enumerator, IEnumerator interrupter,
+			IEnumerator onInterrupt = null)
+		{
+			bool interrupted = false;
+			Coroutine interruptCoroutine = CoroutineRunner.StartCoroutine(EnumeratorCallback(interrupter, () => interrupted = true));
+			yield return WaitForAll(new[]{
+				enumerator,
+				new WaitUntil(() => interrupted)
+			});
+			if (interrupted && onInterrupt != null)
+			{
+				yield return onInterrupt;
+			}
+			if(interruptCoroutine != null){
+				CoroutineRunner.StopCoroutine(interruptCoroutine);
+			}
+		}
+		public static IEnumerator WaitWithInterrupt(IEnumerator enumerator, IEnumerator interrupter,
+			Action onInterrupt = null)
+		{
+			bool interrupted = false;
+			Coroutine interruptCoroutine = CoroutineRunner.StartCoroutine(EnumeratorCallback(interrupter, () => interrupted = true));
+			yield return WaitForAll(new[]{
+				enumerator,
+				new WaitUntil(() => interrupted)
+			});
+			if (interrupted){
+				onInterrupt?.Invoke();
+			}
+			if(interruptCoroutine != null){
+				CoroutineRunner.StopCoroutine(interruptCoroutine);
+			}
+		}
+		public static IEnumerator WaitForOne(IEnumerator[] enumerators)
+		{
+			Coroutine[] coroutines = new Coroutine[enumerators.Length];
+			bool done = false;
+			for (int i = 0; i < enumerators.Length; i++)
+			{
+				coroutines[i] = CoroutineRunner.StartCoroutine(EnumeratorCallback(enumerators[i], () => done = true));
+			}
+			yield return new WaitUntil(() => done);
+			foreach (Coroutine c in coroutines){
+				CoroutineRunner.StopCoroutine(c);
+			}
+		}
+		public static IEnumerator WaitForAll(Coroutine[] coroutines){
 			int i = 0;
 			foreach(Coroutine coroutine in coroutines){
 				CoroutineRunner.StartCoroutine(CoroutineCallback(coroutine, () => i++));
@@ -14,7 +60,7 @@ namespace Lvl3Mage.CoroutineToolkit
 			yield return new WaitUntil(() => i == coroutines.Length);
 		}
 
-		public static IEnumerator WaitAll(IEnumerator[] enumerators){
+		public static IEnumerator WaitForAll(IEnumerator[] enumerators){
 			int i = 0;
 			foreach(IEnumerator enumerator in enumerators){
 				CoroutineRunner.StartCoroutine(EnumeratorCallback(enumerator, () => i++));
